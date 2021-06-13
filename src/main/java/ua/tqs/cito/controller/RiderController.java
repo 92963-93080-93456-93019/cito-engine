305755b90967206e96519219796dd7ba4d6519d0
Tiago Oliveira <tiago.srv.oliveira@ua.pt>
2021-06-13 17:19:32 +0100

    Api documentation done

--- src/main/java/ua/tqs/cito/controller/RiderController.java
+++ src/main/java/ua/tqs/cito/controller/RiderController.java
@@ -1,6 +1,8 @@
 package ua.tqs.cito.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import ua.tqs.cito.service.OrderService;
 import ua.tqs.cito.service.UserRegisterService;
 
 @Controller
+@Tag(name = "Rider", description = "the Rider API")
 @RequestMapping("/riderApi")
 public class RiderController {
 
@@ -20,17 +23,20 @@ public class RiderController {
     private UserRegisterService userRegisterService;
 
     // Rider updates order state
+    @Operation(summary = "Update order status.")
     @GetMapping(value="{riderId}/order/update",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> updateOrder(@PathVariable Long riderId, Long appid, Long orderId, String status){
         return orderService.updateOrder(riderId, appid, orderId, status);
     }
     // Client registers an order
+    @Operation(summary = "Register an order by a client.")
     @PostMapping(value="{clientId}/order/register",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> registerOrder(@PathVariable Long clientId, Long appid, @RequestBody JsonNode payload) {
         System.out.println("Cheguei ao controller");
         return orderService.registerOrder(clientId, appid, payload);
     }
     // Rider registers
+    @Operation(summary = "Register a rider in the platform.")
     @PostMapping(value="/register",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> registerRider(@RequestBody JsonNode payload){
         return userRegisterService.registerRider(payload);

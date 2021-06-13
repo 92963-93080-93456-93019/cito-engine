fe91df934e9c0be679155c53d2c3a609c236571e
Lucas Sousa <joselmdbsousa@ua.pt>
2021-06-13 23:01:36 +0100

    renamed status atribute

--- src/main/java/ua/tqs/cito/controller/ClientController.java
+++ src/main/java/ua/tqs/cito/controller/ClientController.java
@@ -6,7 +6,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ua.tqs.cito.model.Order;
 import ua.tqs.cito.service.OrderService;
 import ua.tqs.cito.service.ProductService;
 import ua.tqs.cito.service.SearchService;

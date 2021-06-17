4861737bf9e3d3bcc94ecc95e7b5e3fe73f3e7e8
Lucas Sousa <joselmdbsousa@ua.pt>
2021-06-18 00:08:12 +0100

    test commit for jacoco

--- src/main/java/ua/tqs/cito/model/App.java
+++ src/main/java/ua/tqs/cito/model/App.java
@@ -23,6 +23,7 @@ public class App {
         this.address = address;
         this.schedule = schedule;
         this.image = image;
+		System.out.println("remove this print")
     }
 
 	public App(Double tax, String name, String address, String schedule, String image) {
